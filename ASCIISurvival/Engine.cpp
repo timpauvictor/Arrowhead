83f93683aa086f2287b4bce19ead3089861bdc22
Timpau, Victor <vtimpau@amd.com>
2020-03-19 15:37:13 -0400

    added player movement

--- ASCIISurvival/Engine.cpp
+++ ASCIISurvival/Engine.cpp
@@ -6,6 +6,8 @@
 int Engine::xRes = 100;
 int Engine::yRes = 50;
 int actorCount = 0;
+int playerX = 40;
+int playerY = 25;
 
 Engine::Engine()
 {
@@ -23,8 +25,15 @@ int Engine::Init()
 	while (!TCODConsole::isWindowClosed()) {
 		TCOD_key_t key;
 		TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
+		switch(key.vk) {
+			case TCODK_UP: playerY--; break;
+			case TCODK_DOWN: playerY++; break;
+			case TCODK_LEFT: playerX--; break;
+			case TCODK_RIGHT: playerX++; break;
+			default: break;
+		}
 		TCODConsole::root->clear();
-		TCODConsole::root->putChar(40, 25, '@');
+		TCODConsole::root->putChar(playerX, playerY, '@');
 		TCODConsole::flush();
 	}
 	return 0;
